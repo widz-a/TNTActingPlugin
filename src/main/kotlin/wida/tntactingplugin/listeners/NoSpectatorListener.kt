b1d88eb13226e606b71f9071e05f040dff2802a2
widz_a_ <itswithermc@gmail.com>
2025-03-30 08:43:08 +0700

    No Spectators!

---
+++ src/main/kotlin/wida/tntactingplugin/listeners/NoSpectatorListener.kt
@@ -0,0 +1,19 @@
+package wida.tntactingplugin.listeners
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+import wida.tntactingplugin.TNTActingPluginMain
+
+class NoSpectatorListener : TNTActingPluginMain.PluginListener() {
+    @EventHandler
+    fun gamemodeSwitch(event: PlayerCommandPreprocessEvent) {
+        val message = event.message
+        if (event.player.hasPermission("tnt.iamallowedtodospectatorbecauseiamcoolaf")) return
+        if (message.startsWith("gamemode spectator") || message.startsWith("gmsp")) {
+            event.isCancelled = true
+            event.player.sendMessage(Component.text("Denied.").color(NamedTextColor.RED))
+        }
+    }
+}
